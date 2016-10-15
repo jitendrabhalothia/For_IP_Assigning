bacacbc94673be77262f06fa31aab2a88dfba0a2
jitendrabhalothia <write2bhalothia@gmail.com>
2016-10-15 20:55:49 +0530

    ip.sh

---
+++ ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bashi
+
+echo  "Getting Public IP of the existence instance and globally exporting variable called ip"
+export ip=`curl  http://169.254.169.254/latest/meta-data/public-ipv4`
+
+echo "Granting Ubuntu as a owner"
+sudo chown ubuntu:ubuntu /usr/share/nginx/html  -R
+cd /usr/share/nginx/html
+
+echo "updating the ip "
+sed -i "s/ip/$ip/g" "index.html"
+
+echo $ip  >hi
